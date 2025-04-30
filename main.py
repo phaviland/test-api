efc878c4e2fb40119420b1b9fe335ceef15ec138
Peter Haviland <phaviland@amalgamatedbenefits.com>
2025-04-30 15:06:26 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from mangum import Mangum
+
+app = FastAPI()
+
+@app.get("/route1")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/route2")
+def read_root():
+    return {"Hello": "World2"}
+
+@app.get("/route3")
+def read_root():
+    return {"Hello": "World3"}
+
+handler = Mangum(app, lifespan="off")
